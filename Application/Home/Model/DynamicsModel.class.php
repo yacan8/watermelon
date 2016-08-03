11e13e9190ce59d4199f4d2aa7cee43b87c2c0db
Selmy <rui08504@163.com>
2016-08-03 19:59:24 +0800

    1

--- Application/Home/Model/DynamicsModel.class.php
+++ Application/Home/Model/DynamicsModel.class.php
@@ -5,8 +5,11 @@ class DynamicsModel extends Model{
 
 	/**
 	 * [getList 获取动态列表]
+	 * @return 动态列表
 	 */
 	public function getList(){
-		
+		$condition['user_id'] = session('userid');
+		$result = $this->where($condition)->select();
+		return $result;
 	}
 }
--- Application/Home/Model/TravelNoteModel.class.php
+++ Application/Home/Model/TravelNoteModel.class.php
@@ -3,12 +3,17 @@ namespace Home\Model;
 use Think\Model;
 class TravelNoteModel extends Model{
 
-
+	/**
+	 * [getList 获取游记列表]
+	 *  @param [Integer] $first 分页参数
+	 *	@param [Interger] $list 分页参数
+	 */
 	public function getList($first,$list){
 		$model = M('TravelNote');
 		$cmodle = D('City');
 		$lmodel = D('Login');
 		$condition['user_id'] = session('userid'); 
+		// $result = $model->limit($first.','.$list)->where($condition)->select();
 		$result = $model->limit($first.','.$list)->select();
 		for ($i=0; $i < 2; $i++) { 
 			preg_match_all('/(?<=original=").+?(?=")/', $result[$i]['content'],$result[$i]['pic']);
@@ -20,6 +25,26 @@ class TravelNoteModel extends Model{
 		return $result;
 	}
 
+	/**
+	 * [getInfoByUserId 获取单条信息]
+	 *	@param [Integer] $userid 用户编号
+	 *	@return 单条游记信息
+	 */
+	public function getInfoByUserId($userid){
+		$condition['user_id'] = $userid; 
+		$result = $this->where($condition)->find();
+		preg_match_all('/(?<=original=").+?(?=")/', $result['content'],$result['pic']);
+		$result['content'] = preg_replace('/<img.+?>/', " ", $result['content']);
+		$city = $cmodle->getById($result['city_id']);
+		$result['city'] = $city['city'];
+		$result['nickname'] = $lmodel->getNickByUserId($result['user_id']);
+		return $result;
+	}
+
+	/**
+	 * [getCount 获取记录总条数]
+	 * @return 记录总条数
+	 */
 	public function getCount(){
 		$condition['user_id'] =	session('userid'); 
 		return $this->count();
